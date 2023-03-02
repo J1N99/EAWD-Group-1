8f7c69e819b17245506d19a3beab0f4e3fe65453
J1N99 <91363943+J1N99@users.noreply.github.com>
2023-03-03 00:46:28 +0800

    Test

--- Code/index.php
+++ Code/index.php
@@ -12,14 +12,13 @@
     <h1>
         <?php
         echo "Hello World!";
-<<<<<<< HEAD
         echo "XXX";
         echo "XXX";
         echo "XXX";
         echo "XXX";
-=======
+
         echo "Hello World!";
->>>>>>> e7400d4c98ca576fe120d3aa0f14a4b60617791c
+
         ?>
     </h1>
     <marquee>SEND HELP!!</marquee>
