33219cff1942567c922de2f27b239172648b25c7
J1N99 <91363943+J1N99@users.noreply.github.com>
2023-03-06 12:30:29 +0800

    new database

--- information file/feedbackdb.sql
+++ information file/feedbackdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2023 at 03:49 AM
+-- Generation Time: Mar 06, 2023 at 05:30 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -20,6 +20,63 @@ SET time_zone = "+00:00";
 --
 -- Database: `feedbackdb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `categories_id` int(11) NOT NULL,
+  `categories` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `comment_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `a_status` int(11) NOT NULL,
+  `commentDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `idea`
+--
+
+CREATE TABLE `idea` (
+  `idea_id` int(11) NOT NULL,
+  `document_url` varchar(100) NOT NULL,
+  `comment_id` int(11) NOT NULL,
+  `a_status` int(11) NOT NULL,
+  `categories_id` int(11) NOT NULL,
+  `closeDate` date NOT NULL,
+  `finalCloseDate` date NOT NULL,
+  `views` int(11) NOT NULL,
+  `submitDate` date NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likepost`
+--
+
+CREATE TABLE `likepost` (
+  `likepost_id` int(11) NOT NULL,
+  `idea_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `t_up` int(11) NOT NULL,
+  `t_down` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -49,6 +106,30 @@ INSERT INTO `user` (`user_id`, `position`, `name`, `department`, `email`, `passw
 --
 
 --
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`categories_id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `idea`
+--
+ALTER TABLE `idea`
+  ADD PRIMARY KEY (`idea_id`);
+
+--
+-- Indexes for table `likepost`
+--
+ALTER TABLE `likepost`
+  ADD PRIMARY KEY (`likepost_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -57,6 +138,24 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `categories_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `likepost`
+--
+ALTER TABLE `likepost`
+  MODIFY `likepost_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
