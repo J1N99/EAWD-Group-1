c821b9c2acf367b8afa38e5e00f6f5485f97b6f5
J1N99 <91363943+J1N99@users.noreply.github.com>
2023-03-06 23:36:58 +0800

    add title on idea

---
+++ Code/list.php
@@ -0,0 +1,35 @@
+<?php
+include("header.php");
+?>
+<label>Filter View:</label>
+<select>
+    <option default>Please select:</option>
+    <option>
+        Most view
+    </option>
+</select>
+
+<label>Filter Thumb-up and Thumb-dowm</label>
+<select>
+    <option default>Please select:</option>
+    <option>
+        Thump-up
+    </option>
+    <option>
+        Thump-down
+    </option>
+</select>
+
+<label>Filter lastest</label>
+<select>
+    <option default>Please select:</option>
+    <option>
+        Thump-up
+    </option>
+    <option>
+        Thump-down
+    </option>
+</select>
+<?php
+include("footer.php");
+?>
