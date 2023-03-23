f12536e6c9b56bc042303aaa0dbc47f6e2a455da
J1N99 <91363943+J1N99@users.noreply.github.com>
2023-03-23 13:00:33 +0800

    department and sign up fix

--- Code/includes/addcategories.inc.php
+++ Code/includes/addcategories.inc.php
@@ -1,13 +1,13 @@
 <?php
 if (isset($_POST['submit'])) {
-    $categories=$_POST['categories'];
+    $categories = $_POST['categories'];
     require_once("dbConnection.inc.php");
     require_once("functions.inc.php");
 
     if (categoriestaken($conn, $categories) !== false) {
-        header("location:../admin/categories.php?error=categoriestaken");
+        header("location:../admin/addcategories.php?error=categoriestaken");
         exit(); //stop
     }
-    
-    createCategories($conn,$categories);
+
+    createCategories($conn, $categories);
 }
